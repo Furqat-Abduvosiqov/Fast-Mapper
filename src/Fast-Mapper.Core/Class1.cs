b56765a0afde61ca01057a5b00aba3f2a3e663c8
Furqat-Abduvosiqov <furqata93@gmail.com>
2025-11-09 00:18:25 +0500

    Init: Initial commit

---
+++ src/Fast-Mapper.Core/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Fast_Mapper.Core;
+
+public class Class1
+{
+}
---
+++ tests/Fast-Mapper.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace Fast_Mapper.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
