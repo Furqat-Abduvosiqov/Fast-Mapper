a8654547a96f9f1c12f40d8cd395fbfe1bdc6da1
Furqat-Abduvosiqov <furqata93@gmail.com>
2025-11-09 12:31:36 +0500

    feat: add expression helper class

---
+++ src/Fast-Mapper.Core/ExpressionsHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace Fast_Mapper.Core;
+
+/// <summary>
+/// Expressions helper class
+/// </summary>
+internal static class ExpressionsHelper
+{
+    public static string GetMemberName<T, TMember>(Expression<Func<T, TMember>> expr)
+    {
+        if (expr.Body is MemberExpression member)
+            return member.Member.Name;
+
+        // handle conversions: dest => (object)dest.Prop
+        if (expr.Body is UnaryExpression { Operand: MemberExpression member2 })
+            return member2.Member.Name;
+
+        throw new ArgumentException("Expression must be a member access", nameof(expr));
+    }
+}
