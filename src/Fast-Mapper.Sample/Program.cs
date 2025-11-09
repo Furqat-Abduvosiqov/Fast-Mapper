6715cf7545a8ddb108f652a175302e12781626a7
Furqat-Abduvosiqov <furqata93@gmail.com>
2025-11-09 12:32:42 +0500

    feat: add sample project with usage of mapper

--- src/Fast-Mapper.Sample/Program.cs
+++ src/Fast-Mapper.Sample/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Fast_Mapper.Core;
 
-Console.WriteLine("Hello, World!");
+namespace Fast_Mapper.Sample;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public Address? Address { get; set; }
+    public string? Password { get; set; }
+}
+
+public class Address { public string? Street { get; set; } public string? City { get; set; } }
+
+public class UserDto
+{
+    public int Id { get; set; }
+    public string? FullName { get; set; }
+    public string? AddressStreet { get; set; }
+    public string? Password { get; set; }
+}
+
+internal static class Program
+{
+    static void Main()
+    {
+        var cfg = new MapperConfig();
+        
+        cfg.CreateMap<User, UserDto>()
+            .ForMember(dest => dest.FullName, src => $"{src.FirstName} {src.LastName}")
+            .ForMember(dest => dest.AddressStreet, src => $"{src.Address?.Street ?? string.Empty} {src.Address?.City ?? string.Empty}")
+            .Ignore(dest => dest.Id)
+            .Ignore(dest => dest.Password);
+
+        var mapper = cfg.BuildMapper();
+
+        var user = new User { Id = 1, FirstName = "John", LastName = "Doe", Address = new Address { Street = "Main", City = "City" }, Password = "secret" };
+        var dto = mapper.Map<User, UserDto>(user);
+
+        Console.WriteLine($"FullName: {dto.FullName}, Street: {dto.AddressStreet}, Id: {dto.Id} , Password: {dto.Password}");
+    }
+}
